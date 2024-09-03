eec9e4bd09a9edbfa2038b3d48cd3a4d37b64222
GianlucaOli <gianlucamarcia123@gmail.com>
2024-09-03 16:58:29 -0300

    Arquivo Git e qualquer coisa.js

--- Git.js
+++ Git.js
@@ -2,9 +2,3 @@ let n1 = 10;
 let n2 = 30;
 const soma = n1 + n2
 console.log(soma);
-//$ git init
-//$ git add .
-//$ git status
-//$ git commit -m <mensagem> "criei o arquivo Git.js"
-//$ git config -- local user.email "gianlucamarcia123@mail.com"
-//$ git config -- local user.name "GianlucaOli"
